4bc507dad88a4f36f9a22ad99adea74b14f43d88
Giampaolo1 <giampaolo.r@outlook.com>
2019-10-16 15:03:03 +0200

    start

---
+++ script/main.js
@@ -0,0 +1,5 @@
+// chiedere nome
+var nome = prompt("Per generare la password Inserisci qui il tuo nome");
+// salutare e chieder cognome
+var saluto = "ciao " + nome;
+// + prompt ("Ora inserisci il tuo cognome")
